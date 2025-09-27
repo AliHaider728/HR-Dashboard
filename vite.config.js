a746467af2af844bee28131ccfcc4a0d255cebd7
ali <ali@gmail.com>
2025-09-27 18:29:21 +0500

    "last commit"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "./", 
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,6 +14,4 @@ export default defineConfig({
       "@/pages": path.resolve(__dirname, "./src/pages"),
     },
   },
-    
-  
- });
+});
