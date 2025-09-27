dbad16c646ce9a30b61129460132e5d54f605333
ali <ali@gmail.com>
2025-09-27 13:29:33 +0500

    "complete"

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,18 @@ export default defineConfig({
     },
   },
   mode: 'production',
-   
-  
- });
+  build: {
+    // Large chunk warning ko manage karne ke liye
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Separate each dependency into its own chunk
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
+});
