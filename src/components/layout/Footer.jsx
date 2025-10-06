0c49c026079d43a04bfca35842662a506ac95b4e
AliHaider728 <decantboy67@gmail.com>
2025-10-06 03:58:31 -0700

    "com"

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-whit text-gray-900 py-4 px-6 flex flex-col sm:text-center   sm:flex-row justify-between items-center ">
+    <footer className="bg-white text-gray-900 py-4 px-6 flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
       <p className="text-sm font-bold font-serif animate-shine bg-clip-text bg-gradient-to-r from-black via-gray-300 to-black bg-[length:200%_auto] text-transparent mb-2 sm:mb-0">
-    2025 © SmartHR.
+        2025 © SmartHR.
       </p>
-      <p className=" font-thin font-serif animate-shine bg-clip-text bg-gradient-to-r from-gray-800 via-gray-300 to-black bg-[length:200%_auto] text-transparent">
-        Designed and developed by <span className="font-bold" > TecnoSphere</span>
+      <p className="font-thin font-serif animate-shine bg-clip-text bg-gradient-to-r from-gray-800 via-gray-300 to-black bg-[length:200%_auto] text-transparent">
+        Designed and developed by <span className="font-bold">TecnoSphere</span>
       </p>
+
       <style>{`
-      
         @keyframes shine {
           0% {
             background-position: 200%;
